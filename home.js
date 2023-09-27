3b8ce9c307651a18a9aaa493b351248518c1564e
rdarshan927 <rdarshan927@gmail.com>
2023-09-27 22:35:43 +0530

    iwtprojectv1.0

---
+++ home.js
@@ -0,0 +1,8 @@
+// Get a reference to the button element by its ID
+var button = document.getElementById("enroll");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    // Display an alert when the button is clicked
+    alert("Button clicked!");
+});
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online teacher Trainer</title>
+</head>
+<body>
+    <p>Hello World!</p>
+</body>
+</html>
